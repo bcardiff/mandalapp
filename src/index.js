6ff44f40710e9ff4e40df4d836dca311e2bce7e3
Brian J. Cardiff <bcardiff@gmail.com>
2017-10-31 00:23:35 -0300

    Refactor pencilCommand. Code cleanup.

--- src/index.js
+++ src/index.js
@@ -7,63 +7,9 @@ import {Handle} from './handle'
 import {HandlesManager} from './handlesManager'
 import {ButtonsManager} from './buttonsManager'
 import {ReplicatorTool} from './replicatorTool'
-import {TraceBuilder} from './traceBuilder'
 import {Button} from './button'
+import {PencilCommand} from './pencilCommand'
 import {ChangeReplicatorsCommand} from './changeReplicatorsCommand'
-
-class SnapHandle extends Handle {
-  coerceCoordinate(point) {
-    return new Point(Math.round(point.x / 10) * 10, Math.round(point.y / 10) * 10)
-  }
-}
-
-const STROKE_LENS_MARGIN = 8
-
-class PencilCommand {
-  constructor(app) {
-    this.tool = new Tool()
-    this.app = app
-
-    this.tool.onMouseMove = (event) => {
-      const point = event.point
-      if (this.shape) {
-        this.shape.position = point
-      } else {
-        this.app.setCursor("none")
-        this.app.drawLayer.activate()
-        this.shape = new Group([
-          new Shape.Circle({center: point, radius: this.app.strokeWidth() + STROKE_LENS_MARGIN, strokeColor: this.app.strokeColor() }),
-          this.app.buildTracePreview(point),
-        ])
-      }
-      this.app.previewAt(point)
-    }
-    this.tool.onMouseDown = (event) => {
-      this.traceBuilder = new TraceBuilder(this.app)
-      this.traceBuilder.append(event.point)
-      this.app.drawingAt(event.point)
-    }
-    this.tool.onMouseDrag = (event) => {
-      this.shape.position = event.point
-      this.traceBuilder.append(event.point)
-      this.app.drawingAt(event.point)
-    }
-    this.tool.onMouseUp = (event) => {
-      this.app.stopDrawing()
-    }
-  }
-
-  activate() {
-    this.tool.activate()
-    this.shape = null;
-  }
-
-  deactivate() {
-    if (this.shape) {
-      this.shape.remove()
-    }
-  }
-}
 
 class CanvasApp {
   constructor() {
