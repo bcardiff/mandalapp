f55437e68d290e992c13be6b130c01c0af8d5c80
Brian J. Cardiff <bcardiff@gmail.com>
2017-11-02 02:14:09 -0300

    Add production configuration

---
+++ webpack.prod.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    index: './index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin("styles.css"),
+    new HtmlWebpackPlugin({
+      title: 'Almandala',
+      template: path.resolve(__dirname, './src/template.ejs')
+    }),
+    new UglifyJSPlugin(),
+  ],
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ["es2016", "react"],
+          plugins: [require('babel-plugin-transform-object-rest-spread')]
+        }
+      }
+    },
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: [{
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      })
+    }]
+  }
+};
